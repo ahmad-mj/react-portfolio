ce7694d3758b2868f74a6736556f9eb90ae7caf7
ahmad <ahmad_almujawer@icloud.com>
2023-12-20 16:20:41 +0100

    Add Routing functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './containers/about';
+import Home from './containers/home';
 
 function App() {
   return (
     <div className="App">
+<Routes>
+  <Route path='/' Component={ Home } />
+  <Route path='/about' Component={ About } />
+</Routes> 
 
     </div>
   );
