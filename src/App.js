36951658aca1778ce361804be619155e5cbd00a7
ahmad <ahmad_almujawer@icloud.com>
2023-12-26 22:07:41 +0100

    Add Particles functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './containers/about';
-import Home from './containers/home';
-import Contact from './containers/contact';
-import Portfolio from './containers/portfolio';
-import Resume from './containers/resume';
-import Skills from './containers/skills';
-import Navbar from './components/navbar';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./containers/about";
+import Home from "./containers/home";
+import Contact from "./containers/contact";
+import Portfolio from "./containers/portfolio";
+import Resume from "./containers/resume";
+import Skills from "./containers/skills";
+import Navbar from "./components/navbar";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useState } from "react";
+import options from "./utils/particles";
 function App() {
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div className="App">
+      if (init)
+      {
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+        />
+      }
       <Navbar />
-<Routes>
-  <Route path='/' Component={ Home } />
-  <Route path='/about' Component={ About } />
-  <Route path='/contact' Component={ Contact } />
-  <Route path='/home' Component={ Home } />
-  <Route path='/portfolio' Component={ Portfolio } />
-  <Route path='/resume' Component={ Resume } />
-  <Route path='/skills' Component={ Skills } />
-</Routes> 
-
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/about" Component={About} />
+        <Route path="/contact" Component={Contact} />
+        <Route path="/home" Component={Home} />
+        <Route path="/portfolio" Component={Portfolio} />
+        <Route path="/resume" Component={Resume} />
+        <Route path="/skills" Component={Skills} />
+      </Routes>
     </div>
   );
 }
