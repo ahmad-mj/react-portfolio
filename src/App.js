9788c0f4ce6377dc8f68aa3780a84e3b252aa81e
ahmad <ahmad_almujawer@icloud.com>
2024-01-18 16:12:09 +0100

    Update import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import About from "./containers/about";
 import Home from "./containers/home";
 import Contact from "./containers/contact";
