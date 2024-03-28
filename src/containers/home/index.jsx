af096949675ffe6d4ec56a088c28ae393766479d
ahmad <ahmad_almujawer@icloud.com>
2024-03-28 13:25:56 +0100

    Render Skills and resume components in Home Page

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import "./styles.scss";
+import Skills from "../skills";
+import Resume from "../resume";
 
 const Home = () => {
 return(
     <>
         <HeroSection/>
+        <Skills/>
+        <Resume/>
     </>
     )
 }
