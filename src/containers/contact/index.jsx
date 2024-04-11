8f3aacd5fb76377719d1c9ece79d0de3d0583ea3
ahmad <ahmad_almujawer@icloud.com>
2024-04-11 12:36:55 +0200

    Add form to contact page

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,8 +1,37 @@
+import "./styles.scss";
+
+
 const Contact = () => {
     return(
-        <div>
-            Contact meme
-        </div>
+<div class="form-container" id="form-container">
+            
+    <form id="booking-form" action="https://your-form-service-url.com/submit" method="post" target="form-iframe">
+    <h3>Kind Information:</h3>
+    <label for="kind-lastname">Nachname:</label>
+    <input type="text" id="kind-lastname" name="kind-lastname" required />
+    
+    <label for="kind-firstname">Vorname:</label>
+    <input type="text" id="kind-firstname" name="kind-firstname" required />
+    
+    <label for="birth-date">Geburtsdatum:</label>
+    <input type="text" id="birth-date" name="birth-date" required />
+    
+    <h3>Elternteil Information:</h3>
+    <label for="parent-lastname">Nachname:</label>
+    <input type="text" id="parent-lastname" name="parent-lastname" required />
+    
+    <label for="parent-firstname">Vorname:</label>
+    <input type="text" id="parent-firstname" name="parent-firstname" required />
+    
+    <label for="email">eMail:</label>
+    <input type="email" id="email" name="email" required />
+    
+    <input type="hidden" name="courseId" value="${courseId}" />
+    
+    <button type="submit">Submit</button>
+  </form>
+        
+  </div>
         )
     }
     export default Contact;
