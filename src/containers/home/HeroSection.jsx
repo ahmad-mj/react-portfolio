bdbe4cda0b210958aea89da8a729f115b3008fb8
ahmad <ahmad_almujawer@icloud.com>
2025-03-31 11:40:50 +0200

    Update Buttons and img

--- src/containers/home/HeroSection.jsx
+++ src/containers/home/HeroSection.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 const HeroSection = () => {
 return(
-    <div className="hero-section">
-    <div className="hero-content">
-      <div className="hero-text">
-        <h1>Welcome to My Website</h1>
-        <p>Learn more about me and my work.</p>
-        <div className="button-container">
-          <button className="btn contact-button">Contact Me</button>
-          <button className="btn cv-button">See CV</button>
-        </div>
+  <section className="hero-section">
+  <div className="hero-content">
+    <div className="hero-text">
+      <h1>Welcome to My Website</h1>
+      <p>Learn more about me and my work.</p>
+      <div className="button-container">
+        <button onClick={() => window.location.href = "#contact"}>Contact Me</button>
+        <button onClick={() => window.open("/resume.pdf", "_blank")}>See CV</button>
       </div>
     </div>
-    <div className="hero-image">
-    <img src="./img/hero_img.jpg" alt="Hero Section" />
-    </div>
   </div>
+  <div className="hero-image">
+    <img src="img/hero_img.jpg" alt="Ahmad" />
+  </div>
+</section>
     )
 }
 export default HeroSection;
