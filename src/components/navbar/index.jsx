b3e44894f6ec779325365f2cc69339e9e81f25f5
ahmad <ahmad_almujawer@icloud.com>
2025-04-07 11:31:59 +0200

    Delete and Clean Up old code. Import and use the ThemeToggle component

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,7 +4,7 @@ import "./styles.scss";
 import { WiEarthquake } from "react-icons/wi";
 import { FaBars } from "react-icons/fa";
 import { HiX } from "react-icons/hi";
-import { useEffect } from "react";
+import ThemeToggle from "./ThemeToggle";
 
 const data = [
   {
@@ -26,41 +26,11 @@ const Navbar = () => {
   const handleMenuClick = () => {
     setMenuIsVisible(false);
   };
-  const switchTheme = (theme) => {
-    const themes = {
-      light: {
-        "--theme-main-color": "#222831",
-        "--theme-background-color": "#ffffff",
-        "--primary_bg": "rgb(200, 200, 200, 87%)",
-      },
-      dark: {
-        "--theme-main-color": "#dddd",
-        "--theme-background-color": "#09666c",
-        "--primary_bg": "rgb(98 45 27 / 87%)",
-      },
-    };
-  
-    // Apply theme colors
-    Object.keys(themes[theme]).forEach((key) => {
-      document.documentElement.style.setProperty(key, themes[theme][key]);
-    });
-  
-    // Save theme preference
-    localStorage.setItem("selectedTheme", theme);
-  };
-  
-  // Load stored theme on startup
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("selectedTheme") || "dark"; // Default to dark mode
-    switchTheme(savedTheme);
-  }, []);
   return (
     <>
       <nav className="navbar">
+        <ThemeToggle />
         <div className="navbar_container">
-        <button onClick={() => switchTheme("light")}>Light Mode</button>
-        <button onClick={() => switchTheme("dark")}>Dark Mode</button>
-
           <Link to={"/"} className="navbar_logo">
             <WiEarthquake size={30} />
           </Link>
@@ -68,7 +38,11 @@ const Navbar = () => {
           <ul className={`navbar_menu ${menuIsVisible && "active"}`}>
             {data.map((item, key) => (
               <li key={key} className="menu_item">
-                <Link to={item.to} className="menu_links" onClick={handleMenuClick}>
+                <Link
+                  to={item.to}
+                  className="menu_links"
+                  onClick={handleMenuClick}
+                >
                   {item.label}
                 </Link>
               </li>
@@ -83,7 +57,6 @@ const Navbar = () => {
         </div>
       </nav>
       <div className={`hero-section ${menuIsVisible ? "menu-active" : ""}`}>
-        {/* Hero content goes here */}
       </div>
     </>
   );
