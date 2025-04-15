48e3ca13444c37dc16e6cc68bd4e558affffb933
ahmad <ahmad_almujawer@icloud.com>
2025-04-15 15:31:54 +0200

    Apply AOS data for fading effect for category

--- src/containers/TrainingPrograms/TrainingPrograms.jsx
+++ src/containers/TrainingPrograms/TrainingPrograms.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHandPeace,
@@ -10,6 +10,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import "./styles.scss";
 import { useTranslation } from "../../hooks/useTranslation";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const iconMap = {
   faHandPeace,
@@ -20,15 +22,18 @@ const iconMap = {
   faBan,
 };
 
-const TrainingPrograms = ({ lang = 'en' }) => {
+const TrainingPrograms = ({ lang = "en" }) => {
   const trainingPrograms = useTranslation(lang).trainingPrograms;
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
 
   return (
     <section id="training" className="skills-section">
       <h2>Training Programs</h2>
       <div className="box">
         {trainingPrograms.map((program, index) => (
-          <div key={index} className="skill-category">
+          <div key={index} className="skill-category" data-aos="fade-up">
             <h3>{program.category}</h3>
             <div className="skills">
               {program.skills.map((skill, i) => (
