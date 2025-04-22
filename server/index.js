e9554b5b46a2594bbbfb0cc84137d219f5aaa607
ahmad <ahmad_almujawer@icloud.com>
2025-04-22 17:49:25 +0200

    Update CORS middleware

--- server/index.js
+++ server/index.js
@@ -2,23 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
-const emailRoute = require("./routes/email");
+const emailRoute = require("./routes/contact");
 
-console.log(typeof emailRoute); // should be 'function'
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+    credentials: false
+}));
 
 //load environment variables
 dotenv.config();
 
 // middelware
 app.use(express.json());
-app.use(cors());
 // basic route
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-app.use("/api/email", emailRoute);
+app.use("/api/contact", emailRoute);
 
 // Start server
-const PORT = process.env.Port || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
