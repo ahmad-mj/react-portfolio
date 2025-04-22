cc6e6b784eded2957ed854bfd2a3800449acc9c3
ahmad <ahmad_almujawer@icloud.com>
2025-04-22 13:53:15 +0200

    Clean spaces and add logs

--- server/routes/email.js
+++ server/routes/email.js
@@ -1,35 +1,38 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const nodemailer = require("nodemailer");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    const { name, email, message } = req.body;
+router.post("/", async (req, res) => {
+    console.log('Request body: ', req.body);
+    
+  const { name, email, message } = req.body;
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    const mailOptions = {
-        form: email,
-        to: process.env.EMAIL_USER,
-        subject: `Dog Training Contact from${name}`,
-        text: message,
-    };
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: `Dog Training Contact from${name}`,
+    text: message,
+  };
 
-    try {
-        await transporter.sendMail(mailOptions);
-        res.status(200).send({success: true, message: 'Email sent successfully!'})
-    } catch (error) {
-        console.error('Error sending email: ',error);
-        res.status(500).send({success: false, message: 'Failed to send email!'})
-    }
+  try {
+    await transporter.sendMail(mailOptions);
+    res
+      .status(200)
+      .send({ success: true, message: "Email sent successfully!" });
+  } catch (error) {
+    console.error("Error sending email: ", error);
+    res.status(500).send({ success: false, message: "Failed to send email!" });
+  }
 });
 
 module.exports = router;
-
