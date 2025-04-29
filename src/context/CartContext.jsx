c1c24f1c2007389a85b9880fd74ec74b0ec349ea
ahmad <ahmad_almujawer@icloud.com>
2025-04-29 14:10:49 +0200

    Store cart state and give every component access

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,4 +1,33 @@
-const CartContext = () => {
-    console.log("From CartContext: Here goes Global cart state")
-}
-export default CartContext;
+import React, {useContext, useReducer,createContext} from "react";
+
+const CartContext = createContext();
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const exists = state.find((item) => item.id === action.payload.id);
+      if (exists) {
+        return state.map((item) =>
+          item.id === action.payload.id ? { ...item, quantity: item + 1 } : item
+        );
+      }
+      return [...state, { ...action.payload, quantity: 1 }];
+    case "REMOVE":
+      return state.filter((item) => item.id !== action.payload);
+    case "CLEAR":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export const CartProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(cartReducer, []);
+  return (
+    <CartContext.Provider value={{ cart, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
