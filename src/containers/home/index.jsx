2a87ec88f0a84a155990903f4fb8f6d66b3b4453
ahmad <ahmad_almujawer@icloud.com>
2025-05-02 17:47:15 +0200

    Delete Footer component from Home

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import HeroSection from "./HeroSection";
-import "./styles.scss";
 import Skills from "../TrainingPrograms/TrainingPrograms";
 import Resume from "../resume";
-import Footer from "./Footer";
 
 const Home = () => {
 return(
@@ -11,7 +9,6 @@ return(
         <HeroSection/>
         <Skills/>
         <Resume/>
-        <Footer/>
     </>
     )
 }
