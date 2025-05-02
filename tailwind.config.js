bdb65e66e9a06aaf91c18d54bc18c88a5fa537fc
ahmad <ahmad_almujawer@icloud.com>
2025-05-02 12:03:56 +0200

    Update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        primary: "#5e3bee",
-        primary_bg: "#ece9f1",
-        mainColor: "#393e46",
-        sideColor: "#eeeeee",
-        lightColor: "#00adb5",
-        darkblue: "#3a4750",
-        github: "#e94e77",
-        white: "#ffffff",
-        black: "#000000",
-      },
-      fontFamily: {
-        base: ["Arial", "sans-serif"]
-      }
+      
     },
   },
   plugins: [],
