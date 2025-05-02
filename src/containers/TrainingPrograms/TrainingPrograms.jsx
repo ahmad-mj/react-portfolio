79eec1f86031302419e87a63c5b4f0894fb9283b
ahmad <ahmad_almujawer@icloud.com>
2025-05-02 13:23:55 +0200

    Refactor TrainingPrograms component: update styles, icons, and layout. Use Tailwind's responsive utilities

--- src/containers/TrainingPrograms/TrainingPrograms.jsx
+++ src/containers/TrainingPrograms/TrainingPrograms.jsx
@@ -1,50 +1,43 @@
 import { React, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHandPeace,
-  faPause,
-  faArrowRightToBracket,
-  faPersonWalking,
-  faVolumeMute,
-  faBan,
-} from "@fortawesome/free-solid-svg-icons";
-import "./styles.scss";
 import { useTranslation } from "../../hooks/useTranslation";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const iconMap = {
-  faHandPeace,
-  faPause,
-  faArrowRightToBracket,
-  faPersonWalking,
-  faVolumeMute,
-  faBan,
-};
-
 const TrainingPrograms = ({ lang = "en" }) => {
   const trainingPrograms = useTranslation(lang).trainingPrograms;
   useEffect(() => {
-    AOS.init({ duration: 1000 });
+    AOS.init({ duration: 1200 });
   }, []);
 
   return (
-    <section id="training" className="skills-section">
-      <h2>Training Programs</h2>
-      <div className="box">
-        {trainingPrograms.map((program, index) => (
-          <div key={index} className="skill-category" data-aos="fade-up">
-            <h3>{program.category}</h3>
-            <div className="skills">
-              {program.skills.map((skill, i) => (
-                <div key={i} className="skill">
-                  <FontAwesomeIcon icon={iconMap[skill.icon]} />
-                  <p>{skill.name}</p>
-                </div>
-              ))}
+    <section className="bg-white py-12">
+      <div className="max-w-6xl mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-8">Work</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {trainingPrograms.map((program, index) => (
+            <div key={index} className="group" data-aos="fade-up">
+              <div className="overflow-hidden">
+                <img
+                  src={program.image}
+                  alt={program.title || "Project Image"}
+                  className="w-full h-auto transform transition-transform duration-300 group-hover:scale-105"
+                />
+              </div>
+              <h3 className="mt-4 text-xl font-semibold">{program.title}</h3>
+              <p className="text-gray-600">{program.category}</p>
+              <div className="mt-2">
+                {program.skills.map((skill, i) => (
+                  <span
+                    key={i}
+                    className="inline-block text-sm text-gray-500 mr-2"
+                  >
+                    {skill.name}
+                  </span>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
