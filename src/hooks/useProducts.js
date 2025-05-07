48d47b625c1d0ef94591ddcc7f60cbb4f1d969b2
ahmad <ahmad_almujawer@icloud.com>
2025-05-07 10:25:44 +0200

    Add custom hook to fetch products

---
+++ src/hooks/useProducts.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const useProducts = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const res = await fetch("/products.json");
+                const data = await res.json();
+                setProducts(data);
+            } catch (error) {
+                console.error("Failed to fetch products", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchProducts();
+    }, [])
+    return { products, loading };
+};
